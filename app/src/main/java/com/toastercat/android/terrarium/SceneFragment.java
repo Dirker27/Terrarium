43356480e5864e8c4cbb91e005d295dab43675bd
Dirk <hortensd@gmail.com>
2016-04-16 01:45:12 -0700

    Added Background Animation

--- app/src/main/java/com/toastercat/android/terrarium/SceneFragment.java
+++ app/src/main/java/com/toastercat/android/terrarium/SceneFragment.java
@@ -1,5 +1,8 @@
 package com.toastercat.android.terrarium;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,7 +12,12 @@ import android.widget.ImageView;
 
 public class SceneFragment extends Fragment {
 
+    private View mSceneView;
     private View mTerrariumView;
+    private View mLawnView;
+
+    private int mBackgroundStart;
+    private int mBackgroundEnd;
 
     public static Fragment newInstance() {
         return new SceneFragment();
@@ -21,12 +29,32 @@ public class SceneFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_scene, container, false);
 
+        this.mSceneView = view.findViewById(R.id.view_scene_container);
         this.mTerrariumView = view.findViewById(R.id.view_terrarium);
+        this.mLawnView = view.findViewById(R.id.component_lawn);
         int terrariumWidth = this.mTerrariumView.getWidth();
         int terrariumHeight = this.mTerrariumView.getHeight();
+
+        Resources resources = getResources();
+        this.mBackgroundStart = resources.getColor(R.color.background);
+        this.mBackgroundEnd = resources.getColor(R.color.colorPrimary);
+
+        this.mSceneView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animate();
+            }
+        });
 
         return view;
     }
 
+    private void animate() {
+        ObjectAnimator sunsetColorAnimator = ObjectAnimator
+                .ofInt(this.mSceneView, "backgroundColor", this.mBackgroundStart, this.mBackgroundEnd)
+                .setDuration(3000);
+        sunsetColorAnimator.setEvaluator(new ArgbEvaluator());
 
+        sunsetColorAnimator.start();
+    }
 }
